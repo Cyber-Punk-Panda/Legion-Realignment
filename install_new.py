21717fd2a78a6786929b8cd4a7990d6ee2295b7b
Quitch <quitch@users.noreply.github.com>
2017-02-26 22:14:21 +0000

    Update install script

--- install_new.py
+++ install_new.py
@@ -14,6 +14,7 @@ import distutils.dir_util
 ROOT_PATH = './' # this expects the script to reside and be executed from within the base mod folder
 
 CLIENT_SUFFIX = '-Client'
+UI_SUFFIX = '-Client-UI'
 SERVER_SUFFIX = '-Server'
 
 root_folder = os.path.realpath(ROOT_PATH)
@@ -26,11 +27,19 @@ try:
     distutils.dir_util.remove_tree(os.path.join(root_folder + CLIENT_SUFFIX))
 except:
     print('No Old Client Folder')
+try:
+    distutils.dir_util.remove_tree(os.path.join(root_folder + UI_SUFFIX))
+except:
+    print('No Old UI Folder')
 print ('Copy Client')
 shutil.copytree(os.path.join(root_folder, 'client'), os.path.join(root_folder + CLIENT_SUFFIX))
+print ('Copy UI')
+shutil.copytree(os.path.join(root_folder, 'branding'), os.path.join(root_folder + UI_SUFFIX))
 print ('Copy Server')
 shutil.copytree(os.path.join(root_folder, 'server'), os.path.join(root_folder + SERVER_SUFFIX))
 print ('Merge Shared into Client')
 distutils.dir_util.copy_tree(os.path.join(root_folder, 'shared'), os.path.join(root_folder + CLIENT_SUFFIX))
+print ('Merge Shared into UI')
+distutils.dir_util.copy_tree(os.path.join(root_folder, 'shared'), os.path.join(root_folder + UI_SUFFIX))
 print ('Merge Shared into Server')
 distutils.dir_util.copy_tree(os.path.join(root_folder, 'shared'), os.path.join(root_folder + SERVER_SUFFIX))
